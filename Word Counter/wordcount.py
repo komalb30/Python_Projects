cb31345fe4d611ce02a490be522df5fb763715fb
Komal Ballewar <100153399+komalb30@users.noreply.github.com>
2025-09-29 14:07:24 +0530

    Add files via upload

--- Word Counter/wordcount.py
+++ Word Counter/wordcount.py
@@ -1,1 +1,8 @@
+def word_counter():
+    text = input("Enter a sentence: ")
+    words = text.split()
+    count = len(words)
+    print(f"Total words: {count}")
 
+if __name__ == "__main__":
+    word_counter()
